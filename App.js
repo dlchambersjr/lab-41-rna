66852280b65e88b13dc182ce187fadf5dc662b5a
David Chambers <dlchambersjr@gmail.com>
2018-12-10 22:23:44 -0800

    transfer from expo io

--- App.js
+++ App.js
@@ -1,57 +1,110 @@
 import * as React from 'react';
-import { PickerIOS, Text, View, StyleSheet } from 'react-native';
-import { Constants } from 'expo';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
+import {
+  TouchableOpacity,
+  Alert,
+  Button,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+import { Speech, Constants } from 'expo';
 
-export default class App extends React.Component {
+import Header from './components/header';
+import Footer from './components/footer';
+
+class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      choice: 'contacts',
+      counter: 0,
+      counterClass: 'positive',
     };
+  }
+
+  handleButtonClick(value) {
+    let counter = this.state.counter + value;
+    if (counter !== 0 && counter % 15 === 0) {
+      Alert.alert('FIZZ BUZZ');
+    } else if (counter !== 0 && counter % 5 === 0) {
+      Alert.alert('BUZZ');
+    } else if (counter !== 0 && counter % 3 === 0) {
+      Alert.alert('FIZZ');
+    }
+    let counterClass = counter >= 0 ? 'positive' : 'negative';
+    this.setState({ counter, counterClass });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>What should I do Today?</Text>
-
-        <PickerIOS
-          selectedValue={this.state.choice}
-          style={{ height: 50, width: 100 }}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({ choice: itemValue })
-          }>
-          <PickerIOS.Item label="Contacts" value="contacts" />
-          <PickerIOS.Item label="Calendar" value="calendar" />
-          <PickerIOS.Item label="Camera" value="camera" />
-        </PickerIOS>
-
-        <Card>
-          <AssetExample />
-        </Card>
+        <TouchableOpacity onPress={() => this.handleButtonClick(1)}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>+</Text>
+          </View>
+        </TouchableOpacity>
+        <Text style={styles.paragraph}>
+          <Text style={styles[this.state.counterClass]}>
+            {this.state.counter}
+          </Text>
+        </Text>
+        <TouchableOpacity onPress={() => this.handleButtonClick(-1)}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>-</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header />
+        <Counter />
+        <Footer />
+      </View>
+    );
+  }
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: 'lightblue',
+    backgroundColor: '#ecf0f1',
     padding: 8,
   },
   paragraph: {
-    margin: 24,
-    fontSize: 18,
+    fontSize: 60,
     fontWeight: 'bold',
     textAlign: 'center',
+    color: 'white',
+  },
+  button: {
+    marginTop: 30,
+    marginBottom: 30,
+    marginHorizontal: 115,
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2196F3',
+  },
+  buttonText: {
+    fontSize: 60,
+    padding: 5,
+    color: 'white',
+  },
+  positive: {
+    backgroundColor: 'green',
+  },
+  negative: {
+    backgroundColor: 'red',
   },
 });
